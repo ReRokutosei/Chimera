2af30a1b82af3d931045ff8ca7f9fcc687867acd
ReRokutosei <35906080+ReRokutosei@users.noreply.github.com>
2026-01-16 15:27:35 +0800

    chore: upgrade AGP to 9.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("io.gitlab.arturbosch.detekt")
     kotlin("plugin.serialization") version "2.3.0"
