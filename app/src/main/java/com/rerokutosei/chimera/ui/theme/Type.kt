83155742cbe68b93ac98a89070c585cb6424a26e
ReRokutosei <35906080+ReRokutosei@users.noreply.github.com>
2026-08-05 16:28:29 +0800

    chore: enforce GPL-3.0-only license in source file headers

--- app/src/main/java/com/rerokutosei/chimera/ui/theme/Type.kt
+++ app/src/main/java/com/rerokutosei/chimera/ui/theme/Type.kt
@@ -4,8 +4,7 @@
  *
  * Licensed under the GNU General Public License v3.0 (the "License");
  * you may redistribute and/or modify this program under the terms of the GNU
- * General Public License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * General Public License as published by the Free Software Foundation, version 3 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
